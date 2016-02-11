2b14b0beb76d9a2821d638d10e89e5dab803a5ec
Kirkhammetz <Neofrascati@hotmail.it>
2016-02-11 14:52:41 +0100

    fixed gulp disaster

--- public/app/team/single.controller.js
+++ public/app/team/single.controller.js
@@ -1,91 +1,75 @@
 angular
-  .module('app')
+.module('app')
+.controller('TeamCtrl', ['$scope', '$stateParams', 'toastr', 'Team', 'Round', 'Player', function($scope, $stateParams, toastr, Team, Round, Player){
+  $scope.team = {};
+  $scope.rounds = undefined;
+  $scope.editMode = false;
 
-  .controller('TeamCtrl', ['$scope', '$stateParams', 'toastr', 'Team', 'Round', 'Player', function($scope, $stateParams, toastr, Team, Round, Player){
-    $scope.team = {};
-    $scope.rounds = undefined;
-    $scope.editMode = false;
+  getTeam();
+  getRounds();
 
-    getTeam();
-    getRounds();
+  $scope.submit = function($files, $event, $flow, team) {
+    $flow.opts.target = '/api/media?type=avatar&team_id=' + team.id;
+    $flow.upload();
+  }
 
-    $scope.submit =  function($files, $event, $flow, team) {
-      $flow.opts.target = '/api/media?type=avatar&team_id=' + team.id;
-      $flow.upload();
+  $scope.editTeam = function(team){
+    Team
+      .edit(team) 
+      .then(function(res){
+        console.log("single.controller.js :14", res.data);
+        toastr.warning('Modificato');
+        getTeam();
+        $scope.editMode = false;
+      },function(err){
+        toastr.error(err, 'Error...');
+      })
+  }
+
+  $scope.deletePlayer = function(player){
+    if(confirm('Sicuro di volerlo rimuovere?')){
+      Player.delete(player)
+      .then(function(res){
+        console.log("player.controller.js :30", res);
+        toastr.warning('Rimosso!');
+        getTeam();
+      }, function(err){
+        toastr.error(err, 'Errore...');
+        console.log("player.controller.js :32", err);
+      })
     }
+  }
 
-    $scope.editTeam = function(team){
-      Team
-        .edit(team) 
-        .then(function(res){
-          console.log("single.controller.js :14", res.data);
-          toastr.warning('Modificato');
-          getTeam();
-          $scope.editMode = false;
-        },function(err){
-          toastr.error(err, 'Error...');
-        })
-    }
+  $scope.createPlayer = function(player){
+    player.team_id = $scope.team.id;
+    Player.create(player)
+    .then(function(res){
+      if(res.status === 200){
+        $scope.player = {};
+        toastr.success('Giocatore creato!');
+        getTeam();
+      }
+    },function(err){
+      console.log("player.controller.js :33", err);
+      toastr.error(err, 'Error...');
+    });
+  }
 
+  function getTeam(){
+    Team
+      .getTeam($stateParams.team_id)
+      .then(function(res){
+        $scope.team = res.data;
+        console.log("single.controller.js :11", res.data);
+      });
+  }
+  function getRounds(){
+    Round
+      .get()
+      .then(function(res){
+        console.log("single.controller.js :66", res.data);
+        $scope.rounds = res.data;
+      });
+  }
 
-    $scope.deletePlayer = function(player){
-      if(confirm('Sicuro di volerlo rimuovere?')){
-        Player.delete(player)
-        .then(function(res){
-          console.log("player.controller.js :30", res);
-          toastr.warning('Rimosso!');
-          getTeam();
-        }, function(err){
-          toastr.error(err, 'Errore...');
-          console.log("player.controller.js :32", err);
-        });
-      }
-  
-  
-      $scope.deletePlayer = function(player){
-        if(confirm('Sicuro di volerlo rimuovere?')){
-          Player.delete(player)
-          .then(function(res){
-            console.log("player.controller.js :30", res);
-            toastr.warning('Rimosso!');
-            getTeam();
-          }, function(err){
-            toastr.error(err, 'Errore...');
-            console.log("player.controller.js :32", err);
-          })
-        }
-      }
-  
-      $scope.createPlayer = function(player){
-        player.team_id = $scope.team.id;
-        Player.create(player)
-        .then(function(res){
-          if(res.status === 200){
-            $scope.player = {};
-            toastr.success('Giocatore creato!');
-            getTeam();
-          }
-        },function(err){
-          console.log("player.controller.js :33", err);
-          toastr.error(err, 'Error...');
-        });
-      }
-  
-      function getTeam(){
-        Team
-          .getTeam($stateParams.team_id)
-          .then(function(res){
-            $scope.team = res.data;
-            console.log("single.controller.js :11", res.data);
-          });
-      }
-      function getRounds(){
-        Round
-          .get()
-          .then(function(res){
-            console.log("single.controller.js :66", res.data);
-            $scope.rounds = res.data;
-          });
-      }
-  
-    }])
+}])
