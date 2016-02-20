fcc4a4497bb47861d8eba9467465b4ddd4d21600
Kirkhammetz <Neofrascati@hotmail.it>
2016-02-20 16:06:05 +0100

    active menu item in admin

--- public/app/admin/admin.controller.js
+++ public/app/admin/admin.controller.js
@@ -1,7 +1,9 @@
 angular.module('app')
 
 .controller('AdminCtrl', ['$auth', '$scope', '$rootScope', '$state', 'Auth', function($auth, $scope, $rootScope, $state, Auth){
-
+  $rootScope.$on('$stateChangeStart',function(e, toState){
+    $scope.adminActive = toState.name;
+  })
   $scope.logout = function(){
     $auth.logout();
     $state.go('public.login');
