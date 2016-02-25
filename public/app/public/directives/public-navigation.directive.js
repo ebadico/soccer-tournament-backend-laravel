8a6e0e3761221ef55e39f18facc592cc48f249f0
Kirkhammetz <Neofrascati@hotmail.it>
2016-02-25 15:22:07 +0100

    homepage 75%

--- public/app/public/directives/public-navigation.directive.js
+++ public/app/public/directives/public-navigation.directive.js
@@ -1,17 +1,23 @@
 angular.module('app')
 .directive('publicNavigation', function(){
   return function(scope, element){
-    
+    var handler = function(){
+      var landingHeight = $('body').find('.landing-container').height();
+      if ($(this).scrollTop() > landingHeight - 25 ){
+          $(element).addClass('nav-dark');
+      }else{
+          $(element).removeClass('nav-dark');
+      }
 
+    }
+
+    $(document).ready(handler);
+    $(window).scroll(handler);
     $(window).scroll(function(){
-        var landingHeight = $('body').find('.landing-container').height();
-        
-        if ($(this).scrollTop() > landingHeight - 25 ){
-            $(element).addClass('nav-dark');
-        }else{
-            $(element).removeClass('nav-dark');
-        }
-    });
+     if($(element).find('.navbar-collapse').hasClass('in')){
+      $('body').find('.navbar-toggle').trigger('click');
+     }
+    })
 
   };
 })
