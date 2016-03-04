f70e0d29364b083e7c61a9351f92f2b8a7f66c52
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-04 18:20:07 +0100

    push

--- public/app/directives/globus-table.directive.js
+++ public/app/directives/globus-table.directive.js
@@ -14,6 +14,7 @@ angular
       
       console.log("DIRECTIVE SCOPE", $scope);
       $transclude($scope, function(clone, scope){
+        scope.$parent = $scope.$parent;
         console.log("TRANSCLUDE SCOPE", scope); 
       })
 
