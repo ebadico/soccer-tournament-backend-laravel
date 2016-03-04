9fe21fb7be29010b744e598a682f56acdfba34fc
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-04 18:19:00 +0100

    push

--- public/app/directives/globus-table.directive.js
+++ public/app/directives/globus-table.directive.js
@@ -6,13 +6,17 @@ angular
     restrict: 'E', // this allows restriction of blink to an HTML element.
     transclude: true, // transclusion instructs angular to embed the original content from the DOM into the resultant outputi
     templateUrl: 'app/directives/templates/globus-table.html',
-    scope: true, // > SHOULD BE FALSE ON THE SERVER BECAUSE FUCK IT OR THE SERVER WILL INHEREDIT FROM PublicCtrl no matter what... :\
+    scope: false, // > SHOULD BE FALSE ON THE SERVER BECAUSE FUCK IT OR THE SERVER WILL INHEREDIT FROM PublicCtrl no matter what... :\
     link: function (scope, el, attr, ctrl, transclude){
       scope.head = attr.head === 'true' ? true : false;
     },
     controller: function($scope, $transclude, Round){
       
-      console.log("globus-table.directive.js :15", $scope.$parent);
+      console.log("DIRECTIVE SCOPE", $scope);
+      $transclude($scope, function(clone, scope){
+        console.log("TRANSCLUDE SCOPE", scope); 
+      })
+
       $scope.ready = true;
 
       $scope.changeRound = function(round){
