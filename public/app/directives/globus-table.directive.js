485b9caafa244cb1c486c1834ddca258a938a04e
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-09 12:15:59 +0100

    update

--- public/app/directives/globus-table.directive.js
+++ public/app/directives/globus-table.directive.js
@@ -9,7 +9,7 @@ angular
     
     // > SHOULD BE TRUE ON THE SERVER BECAUSE FUCK IT OR THE SERVER WILL INHEREDIT FROM PublicCtrl no matter what... :\
     //scope: (window.location.hostname === 'www.globuscup.it' ? true : false), 
-    scope: true,
+    scope: false,
     // WILL FIND A PROPER FIX SOME DAY BUT NOW I DONT HAVE TIME
 
     link: function (scope, el, attr, ctrl, transclude){
