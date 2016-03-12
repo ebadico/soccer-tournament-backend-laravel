ea1934de9a500e80fdb079ad9bf89c8b8c55dd6d
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-12 16:21:04 +0100

    update

--- public/app/filters/filters.js
+++ public/app/filters/filters.js
@@ -17,7 +17,6 @@ angular
 
   .filter('roundFilter', function(){
     return function(array, round_id){
-      array = array || [];
       if(!round_id) return array;
 
       var filtered = array.filter(function(index) {
