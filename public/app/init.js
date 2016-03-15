e726e372ade44b0ea187e8cc2a8a9a4340dc6288
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-15 16:27:05 +0100

    update

--- public/app/init.js
+++ public/app/init.js
@@ -227,6 +227,15 @@ angular.module('app', [
     });
     console.log("init.js :228", "fbsdk_online");
   };
+  
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
   $window.fbAsyncInit();
 
   Round.get()
