988cb0e0191341b9433c36821e073ccbb63f170c
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-15 16:51:35 +0100

    update

--- public/app/public/news/single.controller.js
+++ public/app/public/news/single.controller.js
@@ -3,8 +3,8 @@ angular.module('app')
 .controller('PublicSingleNewsCtrl', ['$rootScope', '$scope', 'ResolvedPost', '$window', function($scope, ResolvedPost, $window){
   $scope.post = ResolvedPost;
 
-  setTimeout(function(){
-    $window.fbAsyncInit();
-  }, 2000);
+  // setTimeout(function(){
+  //   $window.fbAsyncInit();
+  // }, 2000);
 
 }])
