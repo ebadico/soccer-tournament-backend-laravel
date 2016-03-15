d75c303113c03e6c31fe8b8ca07b3578d0d69bab
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-15 16:50:34 +0100

    update

--- public/app/public/news/single.controller.js
+++ public/app/public/news/single.controller.js
@@ -2,14 +2,6 @@ angular.module('app')
 
 .controller('PublicSingleNewsCtrl', ['$rootScope', '$scope', 'ResolvedPost', '$window', function($scope, ResolvedPost, $window){
   $scope.post = ResolvedPost;
-  $scope.CURRENT_URL = $window.location.href;
-  $scope.BASE_URL = $window.location.host;
-
-  $rootScope.FB_post_title = '';
-  $rootScope.FB_post_excerpt = '';
-  $rootScope.FB_post_image = '';
-  $rootScope.FB_post_url = $window.location.href;
-
 
   setTimeout(function(){
     $window.fbAsyncInit();
