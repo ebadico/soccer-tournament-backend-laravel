eefea8f1812de99f0bf3f395a647bdbd308d8fcf
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-16 14:35:55 +0100

    update

--- app/Http/Controllers/TeamCtrl.php
+++ app/Http/Controllers/TeamCtrl.php
@@ -32,16 +32,6 @@ class TeamCtrl extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      * request needs round_id and team name
      */
@@ -82,16 +72,6 @@ class TeamCtrl extends Controller
         return response()->json($data, $status);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
