14a612be24b42dc68abde303a5e606111732844e
Kirkhammetz <Neofrascati@hotmail.it>
2016-03-27 12:35:24 +0200

    x

--- public/static.blade.php
+++ public/static.blade.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>Globuscup 2016</title>
+
+
+    <meta property="og:url"                content="http://www.globuscup.it" />
+    <meta property="og:type"               content="website" />
+    <meta property="og:title"              content="Globuscup News 1" />
+    <meta property="og:description"        content="GlobusCup 2016 Tornei di calcio A5" />
+    <meta property="og:site_name"          content="GlobusCup">
+    <meta property="fb:app_id"             content="1020464471354564">
+    <meta property="og:image"              content="http://www.globuscup.it/images/globus_facebook.jpg" />
+    <meta property="og:rich_attachment"    content="true">
+    <meta property="fb:profile_id"         content="Globus-Cup-499735930135609">
 </head>
 <body>
-  <h1>TEST</h1>
 </body>
 </html>
