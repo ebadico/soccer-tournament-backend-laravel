b536b662a6d3b3061b070e920eed2f22385f1c55
Kirkhammetz <Neofrascati@hotmail.it>
2016-04-04 16:14:46 +0200

    x

--- public/news.blade.php
+++ public/news.blade.php
@@ -11,6 +11,7 @@
     <meta property="fb:app_id"             content="1020464471354564">
 
     <meta property="og:site_name"          content="GlobusCup">
+    <meta property="og:url"                content="www.globuscup.it">
     
 
     <meta property="og:title"              content="{{ $news->title }}" />
